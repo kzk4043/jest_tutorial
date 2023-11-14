035c1f578e71b36b21cf0ef9d51fdd7ff5cfaa65
kzk4043 <okazakishoma@team-lab.com>
2023-11-14 22:22:39 +0900

    feat: add fetch test

--- src/async/fetch.js
+++ src/async/fetch.js
@@ -5,4 +5,24 @@ const fetchTodo = async () => {
   return data;
 }
 
-module.exports = fetchTodo;
+// jsonplaceholderの`/todos/1`を取得して失敗する関数
+const fetchTodoError = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/10000');
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+// jsonplaceholderの`/todos/1`を取得したあとコールバックを実行する関数
+const fetchTodoCallback = (callback) => {
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(data => callback(null, data))
+    .catch(error => callback(error));
+}
+
+module.exports = {fetchTodo, fetchTodoError, fetchTodoCallback};
--- src/async/fetch.test.js
+++ src/async/fetch.test.js
@@ -1,12 +1,71 @@
-const fetchTodo = require('./fetch');
+const { fetchTodo, fetchTodoError, fetchTodoCallback } = require("./fetch");
+
+// fetchTodo正常系テストをPromiseを使って書く
+test("fetchTodo", () => {
+  return fetchTodo().then((data) => {
+    expect(data).toEqual({
+      userId: 1,
+      id: 1,
+      title: "delectus aut autem",
+      completed: false,
+    });
+  });
+});
 
 // fetchTodo関数の正常系テスト
-test('fetchTodo', async () => {
-    const data = await fetchTodo();
+test("fetchTodo", async () => {
+  const data = await fetchTodo();
+  expect(data).toEqual({
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  });
+});
+
+// fetchTodoError関数の異常系テスト
+test("fetchTodoError", async () => {
+  expect.assertions(1);
+  try {
+    await fetchTodoError();
+  } catch (e) {
+    expect(e.message).toMatch("HTTP error! status: 404");
+  }
+});
+
+// callbackは呼ばれずに終わるため、このテストは失敗する
+// 関数がcallback呼んで終わるケースとかあまり見たことがないが…
+xtest('the data is peanut butter', () => {
+  expect.assertions(1);
+
+  function callback(error, data) {
+    if (error) {
+      throw error;
+    }
+
     expect(data).toEqual({
-        userId: 1,
-        id: 1,
-        title: "delectus aut autem",
-        completed: false
+      userId: 1,
+      id: 1,
+      title: "delectus aut autem",
+      completed: false,
     });
+  }
+
+  fetchTodoCallback(callback);
+});
+
+// fetchTodoCallback関数の正常系テスト
+test("fetchTodoCallback", (done) => {
+  expect.assertions(1);
+  const callback = (error, data) => {
+    expect(data).toEqual({
+      userId: 1,
+      id: 1,
+      title: "delectus aut autem",
+      completed: false,
+    });
+    done();
+  };
+
+  fetchTodoCallback(callback);
 });
